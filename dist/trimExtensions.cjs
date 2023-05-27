fac92b7f7db17347e030239da37759b2bbc921f8
Nathan Bolton <github@thinknathan.com>
2023-05-27 09:14:27 -0700

    Remove unnecessary check

--- dist/trimExtensions.cjs
+++ dist/trimExtensions.cjs
@@ -6,10 +6,7 @@ module.exports = {
 
 		for (const file of result) {
 			const fileExtension = file.outputPath.split('.').pop();
-			// If the file has the extension ".lua" AND other extensions
-			// then delete the final ".lua".
-			// Otherwise, leave the ".lua" extension untouched.
-			if (fileExtension === 'lua' && file.outputPath.split('.').length > 2) {
+			if (file.outputPath.split('.').length > 2) {
 				file.outputPath = file.outputPath.slice(0, -(fileExtension.length + 1));
 			}
 		}
